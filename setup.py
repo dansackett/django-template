64699bd754ef6844014baa9fa2e60a4d6ad50c42
Dan Sackett <danesackett@gmail.com>
2013-08-28 14:39:06 -0400

    Let the good times roll?

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
+from random import SystemRandom
 from subprocess import call
-import random
 
 
 def install_reqs():
@@ -8,7 +8,8 @@ def install_reqs():
 
 
 def make_local_settings():
-    SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+    CHOICES = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    SECRET_KEY = ''.join([SystemRandom().choice(CHOICES) for i in range(50)])
 
     call("cp {{ project_name }}/settings/local.py.example {{ project_name }}/settings/temp_local.py", shell=True)
     call("echo 'SECRET_KEY'='{}' >> myproject/settings/temp_local.py".format(SECRET_KEY), shell=True)
@@ -16,7 +17,7 @@ def make_local_settings():
 
 def update_virtualenv_hooks():
     call("sed 's/PROJECT_NAME/{{ project_name }}/' bin/temp_postactivate > bin/postactivate", shell=True)
-    call(". bin/postactivate", shell=True)
+    call("rm bin/temp_postactivate", shell=True)
 
 
 def setup_database():
@@ -28,7 +29,9 @@ def setup_database():
     call("sed 's/DATABASE_NAME/{}/' {{ project_name }}/settings/temp_dev.py > {{ project_name }}/settings/dev.py".format(local_db_name), shell=True)
     call("sed 's/DATABASE_USER/{}/' {{ project_name }}/settings/temp_dev.py > {{ project_name }}/settings/dev.py".format(local_db_user), shell=True)
     call("sed 's/DATABASE_USER/{}/' {{ project_name }}/settings/temp_local.py > {{ project_name }}/settings/local.py".format(local_db_pwd), shell=True)
-    print "\nLocal DB variables setup\n"
+    call("rm {{ project_name }}/settings/temp_dev.py", shell=True)
+    call("rm {{ project_name }}/settings/temp_local.py", shell=True)
+    print "\n...Local DB variables setup\n"
 
     print "Let's setup the production MySQL Database:"
     prod_db_name = raw_input('Production Database Name: ')
@@ -36,11 +39,13 @@ def setup_database():
 
     call("sed 's/DATABASE_NAME/{}/' {{ project_name }}/settings/temp_prod.py > {{ project_name }}/settings/prod.py".format(prod_db_name), shell=True)
     call("sed 's/DATABASE_USER/{}/' {{ project_name }}/settings/temp_prod.py > {{ project_name }}/settings/prod.py".format(prod_db_user), shell=True)
-    print "\nProduction DB variables setup"
+    call("rm {{ project_name }}/settings/temp_prod.py", shell=True)
+    print "\n...Production DB variables setup"
     print "NOTE: You will need to update your local.py file on production with the correct password.\n"
 
 
 def syncdb():
+    call(". bin/postactivate", shell=True)
     call("django-admin.py syncdb --migrate", shell=True)
 
 
