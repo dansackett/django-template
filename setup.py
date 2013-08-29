83327709f02d40d90cd6231d03437d9127a3958a
Dan Sackett <danesackett@gmail.com>
2013-08-29 12:39:49 -0400

    Trying to eliminate a step for local settings...

--- setup.py
+++ setup.py
@@ -7,14 +7,6 @@ def install_reqs():
     call("pip install -r reqs/dev.txt", shell=True)
 
 
-def make_local_settings():
-    CHOICES = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-    SECRET_KEY = ''.join([SystemRandom().choice(CHOICES) for i in range(50)])
-
-    call("cp {{ project_name }}/settings/local.py.example {{ project_name }}/settings/temp_local.py", shell=True)
-    call("sed 's/_SECRET_KEY/{}/' {{ project_name }}/settings/temp_local.py > {{ project_name }}/settings/temp_local2.py".format(SECRET_KEY), shell=True)
-
-
 def update_virtualenv_hooks():
     call("sed 's/PROJECT_NAME/{{ project_name }}/' bin/temp_postactivate > bin/postactivate", shell=True)
     call("rm bin/temp_postactivate", shell=True)
@@ -27,7 +19,7 @@ def setup_database():
     local_db_pwd = raw_input('Local Database Password: ')
 
     call("sed 's/DATABASE_NAME/{}/g;s/DATABASE_USER/{}/g' {{ project_name }}/settings/temp_dev.py > {{ project_name }}/settings/dev.py".format(local_db_name, local_db_user), shell=True)
-    call("sed 's/DATABASE_PASSWORD/{}/' {{ project_name }}/settings/temp_local2.py > {{ project_name }}/settings/local.py".format(local_db_pwd), shell=True)
+    call("sed 's/DATABASE_PASSWORD/{}/g;s/_SECRET_KEY/{}/g' {{ project_name }}/settings/temp_local.py > {{ project_name }}/settings/local.py".format(local_db_pwd, {{ secret_key }}), shell=True)
     call("rm {{ project_name }}/settings/temp_dev.py", shell=True)
     call("rm {{ project_name }}/settings/temp_local.py", shell=True)
     call("rm {{ project_name }}/settings/temp_local2.py", shell=True)
@@ -47,11 +39,6 @@ if __name__ == "__main__":
     print "\nInstalling Requirements"
     print "===========================\n"
     install_reqs()
-    print "...done!"
-
-    print "\nUpdating Local Settings"
-    print "===========================\n"
-    make_local_settings()
     print "...done!"
 
     print "\nUpdating Virtualenv Hook"
